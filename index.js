66d34aafb894d12209b120f071ff6610cd6a184d
Alexander <abertholf7626@floridapoly.edu>
2019-09-18 21:04:14 -0400

    Added Command Handler

--- index.js
+++ index.js
@@ -7,12 +7,18 @@ client.config = botconfig;
 
 
 client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
 
 config({
   path: __dirname + "/.env"
 })
 
-fs.readdir("./Commands/", (err, files) => {
+let handlers = ["Commands.js"];
+handlers.forEach(handler => {
+    require(`./Utils/${handler}`)(client);
+});
+
+/*fs.readdir("./Commands/", (err, files) => {
   if(err) console.log(err);
 
   if(files.length <= 0){
@@ -21,6 +27,7 @@ fs.readdir("./Commands/", (err, files) => {
   }
 
   let folders = files.filter(f => f.split(".")[1] !== "js");
+  let js = files.filter(f => f.split(".")[1] === "js");
 
   folders.forEach((r, i) => {
     fs.readdir(`./Commands/${r}`, (err, files) => {
@@ -29,11 +36,19 @@ fs.readdir("./Commands/", (err, files) => {
         let prop = require(`./Commands/${r}/${f}`);
         if(!prop) return;
         client.commands.set(prop.help.name, prop);
+        js = js.filter(q => q !== f);
         console.log(`Loaded: ${f}`);
       })
     })
   })
-})
+
+  js.forEach((r, i) => {
+    let prop = require(`./Commands/${r}`);
+    if(!prop) return;
+    client.commands.set(prop.help.name, prop);
+    console.log(`Loaded: ${r}`);
+  })
+})*/
 
 client.on("ready", () => {
   console.log(`${client.user.tag} is online`);
@@ -52,8 +67,8 @@ client.on("message", (message) => {
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
 
-  let command = client.commands.get(cmd.slice(1));
-  if(!command){
+  let command = client.commands.get(cmd.slice(prefix.length) || client.aliases.get(cmd.slice(prefix.length)));
+  if(!command || !message.content.startsWith(prefix)){
     return;
   }
   try{
