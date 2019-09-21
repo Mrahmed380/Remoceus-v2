6b8031fbcccbe8288d99f7edb578b57745bde8c5
Alexander <abertholf7626@floridapoly.edu>
2019-09-20 21:21:17 -0400

    Added Kill.js
    Allows the bot owner to take the bot offline

---
+++ Commands/Owner/Kill.js
@@ -0,0 +1,17 @@
+
+module.exports = {
+  name: "kill",
+  aliases: [],
+  category: "Owner",
+  description: "Takes the bot offline (Bot owner only)",
+  usage: "",
+  run: async (client, message, args) => {
+    if(message.deletable) message.delete();
+    if(message.author.id !== client.config.botowner) return;
+    message.channel.send("Signing Off")
+    .then(m => {
+      return client.destroy();
+    })
+    .catch(err => console.log(err));
+  }
+}
