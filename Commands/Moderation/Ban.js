7b6784ac453d373911a6e1049ad56ff2f8a98687
Alexander <abertholf7626@floridapoly.edu>
2019-09-21 13:59:15 -0400

    Added New Command
    -ban.js

---
+++ Commands/Moderation/Ban.js
@@ -0,0 +1,63 @@
+
+const { RichEmbed } = require("discord.js");
+
+module.exports = {
+  name: "ban",
+  aliases: [],
+  category: "Moderation",
+  description: "Bans a user",
+  usage: "<@user> <reason>",
+  run: async (client, message, args) => {
+    if(message.deletable) message.delete();
+
+    if(!args[0]){
+      return message.channel.send("You need to mention another user").then(m => m.delete(5000));
+    }
+
+    let reason = args.slice(1).join(" ") || "No Reason Given";
+
+    let toBan = message.mentions.members.first();
+
+    if(!toBan){
+      return message.channel.send("Could not find user").then(m => m.delete(5000));
+    }
+
+    if(!message.member.hasPermission("BAN_MEMBERS", false, true, true)){
+      return client.errors.noPerms(message, "Ban Members");
+    }
+
+    if(!message.guild.me.hasPermission("BAN_MEMBERS", false, true, true)){
+      return message.channel.send("Sorry, but I don't have permission to ban members").then(m => m.delete(5000));
+    }
+
+    if(toBan.id === message.author.id){
+      return message.channle.send("You cannot ban yourself").then(m => m.delete(5000));
+    }
+
+    if(toBan.id === client.user.id){
+      return message.channel.send("You cannot ban me");
+    }
+
+    if(!toKick.bannable){
+      return message.channel.send("I cannot ban this member");
+    }
+
+    let banChannel = message.guild.channels.find(channel => channel.name === "logs") || message.channel;
+
+    const banEmbed = new RichEmbed()
+    .setTitle("Ban Embed")
+    .setThumbnail(toBan.user.displayAvatarURL)
+    .setColor(client.config.color)
+    .addField("Banned User", `${toKick.user.tag} (${toKick.id})`)
+    .addField("Banned By", `${message.author.tag} (${message.author.id})`);
+    if(reason){
+      kickEmbed.addField("Reason", reason);
+    }
+
+    toKick.ban(reason)
+      .then(() =>{
+        return banChannel.send(banEmbed)
+      })
+      .catch(err => console.log(err));
+  }
+}
