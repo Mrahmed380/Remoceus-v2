72765eb01f6491269fb59e914e7ea6a9f28ad5e4
Alexander <abertholf7626@floridapoly.edu>
2019-09-20 21:18:39 -0400

    Update Errors.js
    Added noPerms function

--- Utils/Errors.js
+++ Utils/Errors.js
@@ -3,5 +3,9 @@ module.exports = {
   noUser: (message) => {
     message.channel.send("No user found")
       .then(m => m.delete(5000));
+  },
+  noPerms: (message, perm) => {
+    message.channel.send(`Missing Permission: ${perm}`)
+      .then(m => m.delete(5000));
   }
 }
