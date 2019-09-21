555f83839cfa339b4da3bfe3d189ee94d7fb578f
Alexander <abertholf7626@floridapoly.edu>
2019-09-21 14:00:49 -0400

    Update Clear.js
    -updated usage
    -uses client.errors
    -added periods
    -now checks for if number entered is less than 0

--- Commands/Moderation/Clear.js
+++ Commands/Moderation/Clear.js
@@ -4,12 +4,13 @@ module.exports = {
   aliases: [],
   category: "Moderation",
   description: "Clears up to 100 messages at a time",
-  usage: "",
+  usage: "<number between 0 and 100>",
   run: async (client, message, args) => {
     if(message.deletable) message.delete();
-    if(!message.member.hasPermission("MANAGE_MESSAGES")) return client.errors.noPerms(message,"Manage Messages");
-    if(!args[0] || isNaN(args[0])) return message.channel.send("You need to enter a number").then(m => m.delete(5000).catch(err => {}));
-    if(args[0]>100) return message.channel.send("Cannot delete more than 100 messages at a time").then(msg => msg.delete(5000).catch(err => {}));
+    if(!message.member.hasPermission("MANAGE_MESSAGES", false, true, true)) return client.errors.noPerms(message,"Manage Messages");
+    if(!args[0] || isNaN(args[0])) return message.channel.send("You need to enter a number.").then(m => m.delete(5000).catch(err => {}));
+    if(args[0]>100) return message.channel.send("Cannot delete more than 100 messages at a time.").then(msg => msg.delete(5000).catch(err => {}));
+    if(args[0]<=0) return message.channel.send("Must enter a number greater than 0.").then(msg => msg.delete(5000).catch(err => {}));
     message.channel.bulkDelete(args[0], true).then(messages => {
       return message.channel.send(`Cleared ${messages.size} messages`);
     })
