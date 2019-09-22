39de53a79ea07a6c8fd0e7f9aa6f445ab3d3b96a
Alexander <abertholf7626@floridapoly.edu>
2019-09-22 14:20:52 -0400

    Added Change Logs
    - Keeps track of changes

---
+++ Utils/ChangeLogs.js
@@ -0,0 +1,35 @@
+module.exports = {
+  "1.0.0": [
+    "Initial Release"
+  ],
+  "1.1.0": [
+    "Added Command Handler",
+    "Added Kick Command",
+    "Added Friend Code Sharing",
+    "Added Help Command"
+  ],
+  "1.2.0": [
+    "Added Bot Info Command",
+    "Added Presence to Remoceus",
+    "Added Server Info Command",
+    "Added Members Command",
+    "Added Clear Command",
+    "Added Kill Command",
+    "Commands are now displayed in title case in !help",
+    "Added Ban Command",
+    "!help now displays required permissions for a command",
+    "Various under the hood changes"
+  ],
+  "1.3.0": [
+    "Added GetPermissions Command",
+    "Added Pokemon Command",
+    "Added Learnsets Command",
+    "Changed how commands are displayed in !help"
+  ],
+  "1.4.0": [
+    "Added Moves Command",
+    "Added Changes Logs",
+    "!botinfo now displays the most recent changes",
+    "Added ChangeLogs Command"
+  ]
+}
