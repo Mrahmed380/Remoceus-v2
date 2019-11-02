2c44815b6f909a3430f73b19025bb30ae4f43cc9
Alexander <abertholf7626@floridapoly.edu>
2019-11-02 13:42:44 -0400

    Update 1.6.0
    - Added MongoDB support
    - Updated FC related Commands
    - Update Index.js

--- Commands/Pokemon/SetFC.js
+++ Commands/Pokemon/SetFC.js
@@ -1,27 +1,31 @@
-const info = require("../../League Files/Info.json");
-const fs = require("fs");
-
+const FC = require("../../Models/FC.js");
 
 module.exports = {
-  name: "setfc",
-  aliases: [],
-  category: "Pokemon",
-  description: "Sets your fc",
-  usage: "<fc> (ex: !setfc 3883-7141-8049)",
-  permissions: [],
-  run: async (client, message, args) => {
-    let fc = args.join(" ");
-    if(!info[message.author.id]){
-      info[message.author.id] = {
-        fc: fc,
-        ign: "Not set, use !setign <ign> to set your IGN"
-      };
-    }else{
-      info[message.author.id].fc = fc;
-    }
-    fs.writeFile('./League Files/Info.json', JSON.stringify(info), (err) => {
-      if(err) console.log(err);
-    })
-    message.channel.send(`Set ${message.author.username}'s fc to ${fc}`);
-  }
+	name: "setfc",
+	aliases: [],
+	category: "Pokemon",
+	description: "Sets your Friend Code for !fc command",
+	usage: "<fc>",
+	permissions: [],
+	run: async (client, message, args) => {
+		if(message.deletable) message.delete();
+		let myNewFC = args.length > 0 ? args.join(" "): "No FC set, use !setfc <fc> to set your fc (ex. !setfc 3883-7141-8049)";
+		FC.findOne({
+			userID: message.author.id,
+		}, (err, fc) => {
+			if(err) console.log(err);
+			if(!fc){
+				const newFC = new FC({
+					userID: message.author.id,
+					fc: myNewFC,
+					ign: "No IGN set, use !setign <ign> to set your ign (ex. !setign Thot Slayer)"
+				});
+				newFC.save().catch(err => console.log(err));
+			}else{
+				fc.fc = myNewFC;
+				fc.save().catch(err => console.log(err));
+			}
+			message.channel.send(`Set Friend Code to: ${myNewFC}`);
+		})
+	}
 }
